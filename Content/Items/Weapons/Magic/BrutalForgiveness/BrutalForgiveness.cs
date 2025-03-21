9bdf1f0a03a127f9b050c7d8cdba0e4ea94eac49
Dominic Karma <farfetchedking541@gmail.com>
2025-03-21 14:18:11 -0500

    Make Brutal Forgiveness a Nameless drop

--- Content/Items/Weapons/Magic/BrutalForgiveness/BrutalForgiveness.cs
+++ Content/Items/Weapons/Magic/BrutalForgiveness/BrutalForgiveness.cs
@@ -1,5 +1,9 @@
-﻿using NoxusBoss.Content.Rarities;
+﻿using HeavenlyArsenal.Core.Globals;
+using NoxusBoss.Content.NPCs.Bosses.NamelessDeity;
+using NoxusBoss.Content.Rarities;
+using NoxusBoss.Core.GlobalInstances;
 using Terraria;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -7,6 +11,26 @@ namespace HeavenlyArsenal.Content.Items.Weapons.Magic.BrutalForgiveness;
 
 public class BrutalForgiveness : ModItem
 {
+    public override void SetStaticDefaults()
+    {
+        GlobalNPCEventHandlers.ModifyNPCLootEvent += (NPC npc, NPCLoot npcLoot) =>
+        {
+            if (npc.type == ModContent.NPCType<NamelessDeityBoss>())
+            {
+                LeadingConditionRule normalOnly = new LeadingConditionRule(new Conditions.NotExpert());
+                {
+                    normalOnly.OnSuccess(ItemDropRule.Common(Type));
+                }
+                npcLoot.Add(normalOnly);
+            }
+        };
+        ArsenalGlobalItem.ModifyItemLootEvent += (Item item, ItemLoot loot) =>
+        {
+            if (item.type == NamelessDeityBoss.TreasureBagID)
+                loot.Add(ItemDropRule.Common(Type));
+        };
+    }
+
     public override void SetDefaults()
     {
         Item.width = 12;
