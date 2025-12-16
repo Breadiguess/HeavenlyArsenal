2bdfd587f8d0d00951ca84ea3bc494f87e94a7e2
shnakamura <joaovtnakamura@gmail.com>
2025-12-16 04:08:30 -0300

    feat: refactors the rift eclipse biome

---
+++ Content/Biomes/RiftEclipseBestiaryBackgroundProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using HeavenlyArsenal.Content.NPCs.Hostile.BloodMoon;
+using ReLogic.Content;
+using Terraria.GameContent.Bestiary;
+using Terraria.UI;
+
+namespace HeavenlyArsenal.Content.Biomes;
+
+public sealed class RiftEclipseBestiaryBackgroundProvider : IBestiaryInfoElement, IBestiaryBackgroundImagePathAndColorProvider
+{
+    Asset<Texture2D> IBestiaryBackgroundImagePathAndColorProvider.GetBackgroundImage()
+    {
+        var asset = Main.Assets.CreateUntracked<Texture2D>
+        (
+            new MemoryStream(),
+            $"{nameof(RiftEclipseBestiaryBackgroundSystem)}.BestiaryBackground"
+        );
+
+        return RiftEclipseBestiaryBackgroundSystem.BestiaryBackground != null ? asset : Main.Assets.Request<Texture2D>("Images/MapBG1");
+    }
+
+    Color? IBestiaryBackgroundImagePathAndColorProvider.GetBackgroundColor()
+    {
+        return Color.Black;
+    }
+
+    UIElement IBestiaryInfoElement.ProvideUIElement(BestiaryUICollectionInfo info)
+    {
+        return null;
+    }
+}
---
+++ Content/Biomes/RiftEclipseBiome.cs
@@ -0,0 +1,20 @@
+﻿using Terraria.Localization;
+
+namespace HeavenlyArsenal.Content.Biomes;
+
+public sealed class RiftEclipseBiome : ModBiome
+{
+    public override Color? BackgroundColor { get; } = Color.Black;
+
+    public override string BestiaryIcon { get; } = $"{nameof(HeavenlyArsenal)}/Content/Biomes/RiftEclipseBiome";
+    
+    public override float GetWeight(Player player)
+    {
+        return 0f;
+    }
+    
+    public override bool IsBiomeActive(Player player)
+    {
+        return false;
+    }
+}
