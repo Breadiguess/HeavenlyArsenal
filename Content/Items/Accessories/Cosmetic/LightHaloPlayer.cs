0c53f5194153e1a58d3e6a8c2607277a3c691e44
shnakamura <joaovtnakamura@gmail.com>
2025-12-17 13:57:47 -0300

    feat: refactors the light halo

---
+++ Content/Items/Accessories/Cosmetic/LightHaloPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace HeavenlyArsenal.Content.Items.Accessories.Cosmetic;
+
+public sealed class LightHaloPlayer : ModPlayer
+{
+    /// <summary>
+    ///     Gets or sets whether the Light Halo effects are enabled.
+    /// </summary>
+    public bool Enabled { get; set; }
+
+    public override void ResetEffects()
+    {
+        base.ResetEffects();
+        
+        Enabled = false;
+    }
+}
