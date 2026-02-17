48cec2c102f0f7eb77692072890eb31f6956a8f0
Bread <gamergreen742@gmail.com>
2026-02-17 11:04:13 -0700

    A

---
+++ Core/HitSquadStructure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeavenlyArsenal.Core
+{
+    public class HitSquadStructure
+    {
+        public int[] npcTypes;
+        public HitSquadStructure(int[] npcTypes)
+        {
+            this.npcTypes = npcTypes;
+        }
+    }
+}
---
+++ Core/HitSquadSystem.cs
@@ -0,0 +1,20 @@
+﻿using HeavenlyArsenal.Content.NPCs.Hostile.BloodCult.FleshkinAcolyte_Assassin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeavenlyArsenal.Core
+{
+    internal class HitSquadSystem : ModSystem
+    {
+        public HitSquadStructure CreateHitSquad()
+        {
+            return new HitSquadStructure(new int[]
+            {
+                ModContent.NPCType<FleshkinAcolyte_Assassin>()
+            });
+        }
+    }
+}
