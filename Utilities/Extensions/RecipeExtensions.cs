765e459ad0b4fbacc74d46bd1c39c1f3827be508
shnakamura <joaovtnakamura@gmail.com>
2026-02-18 05:40:24 -0300

    docs: documentation for recipe extensions

--- Utilities/Extensions/RecipeExtensions.cs
+++ Utilities/Extensions/RecipeExtensions.cs
@@ -1,14 +1,27 @@
 ﻿namespace HeavenlyArsenal.Utilities.Extensions;
 
+/// <summary>
+///     Provides <see cref="Recipe" /> extension methods.
+/// </summary>
 public static class RecipeExtensions
 {
+    /// <summary>
+    ///     Attempts to add an ingredient to the given <see cref="Recipe" />.
+    /// </summary>
+    /// <param name="recipe">The <see cref="Recipe" /> to add the ingredient to.</param>
+    /// <param name="modName">The internal name of the mod containing the item.</param>
+    /// <param name="itemName">The internal name of the item to add as an ingredient.</param>
+    /// <remarks>
+    ///     If the specified mod or item cannot be found, this method will fail silently and not add any
+    ///     ingredient to the recipe.
+    /// </remarks>
     public static void TryAddIngredient(this Recipe recipe, string modName, string itemName)
     {
         if (!ModLoader.TryGetMod(modName, out var mod) || !mod.TryFind<ModItem>(itemName, out var item))
         {
             return;
         }
-        
+
         recipe.AddIngredient(item.Type);
     }
 }
